664b798ed22417e87d78b8f651ed6b4e747df96a
ik4rd <treep.me@yandex.ru>
2026-06-15 13:51:12 +0300

    fix: refactoring

--- internal/colormap/algorithms/bsp.go
+++ internal/colormap/algorithms/bsp.go
@@ -13,21 +13,27 @@ func bspSplit(_ *colormap.Pixels, r colormap.Rect, opts colormap.Options) []colo
 		if r.W < n*opts.MinSize {
 			return nil
 		}
+
 		xs := splitAxis(r.X, r.W, n)
 		rects := make([]colormap.Rect, n)
+
 		for i := range n {
 			rects[i] = colormap.Rect{X: xs[i], Y: r.Y, W: xs[i+1] - xs[i], H: r.H}
 		}
+
 		return rects
 	}
 
 	if r.H < n*opts.MinSize {
 		return nil
 	}
+
 	ys := splitAxis(r.Y, r.H, n)
 	rects := make([]colormap.Rect, n)
+
 	for i := range n {
 		rects[i] = colormap.Rect{X: r.X, Y: ys[i], W: r.W, H: ys[i+1] - ys[i]}
 	}
+
 	return rects
 }
